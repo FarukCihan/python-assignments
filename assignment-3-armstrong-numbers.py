8546c0854ef6e55190b1172b97a8a4d76efd197a
FarukCihan <farukcihan20@gmail.com>
2020-07-10 11:13:16 +0300

    assignment-3 modified and completed

--- assignment-3-armstrong-numbers.py
+++ assignment-3-armstrong-numbers.py
@@ -1,25 +1,19 @@
-number = input("Please enter a number to check if it's an Armstrong Number : ")
+while True:
+    number = input("Please enter a number to check if it's an Armstrong Number : ")
 
-len_num = len(number)
+    len_num = len(number)
 
-list_num = list(number)
+    list_num = list(number)
 
-armstrong = 0
+    armstrong = 0
 
-if number.isalpha():
-    print("Do not use any entries other than numeric values")
-    
-elif number.startswith("-"):
-    print("Please enter a positive number")
-    
-elif ("." in list_num) or ("," in list_num):
-    print("Please enter an integer number")
-    
-else:
-    for i in list_num:
-        armstrong += int(i) ** len_num
-        
-    if int(number)==armstrong:
-        print("{} is an Armstrong number".format(int(number)))
-    else:   
-        print("{} is not an Armstrong number".format(int(number)))
+    if number.isdecimal():
+        for i in list_num:
+            armstrong += int(i) ** len_num
+            
+        if int(number)==armstrong:
+            print("{} is an Armstrong number".format(int(number)))
+        else:   
+            print("{} is not an Armstrong number".format(int(number)))
+    else:
+        print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
