f9bdbc6be969a7c45bcfaf6ff236eb3a465b6f61
FarukCihan <farukcihan20@gmail.com>
2020-07-16 12:08:07 +0300

    python assignment-6 completed

---
+++ assignment-6-prime-number-limit.py
@@ -0,0 +1,12 @@
+
+n = int(input("Please enter a integer number to list Prime Numbers : "))
+
+prime = []
+
+for i in range(2,n+1):
+    if i==2 or i==3 or i==5 or i==7:
+        prime.append(i)
+    elif i%2 and i%3 and i%5 and i%7:
+        prime.append(i)
+
+print(prime)
